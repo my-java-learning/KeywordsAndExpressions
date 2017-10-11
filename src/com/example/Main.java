76832ca06ca2df1ef7e1c51c7b502ff886a3a12e
Qsior <lukaszbojes@gmail.com>
2017-10-11 17:16:49 +0200

    Initial commit

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,13 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int int2 = 5;
+
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if(highScore == 50){
+            System.out.println("This is an expression");
+        }
     }
 }
